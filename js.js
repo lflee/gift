ef27e8c591d3f7b5078d81fc2102689f748623b7
峰 <leelapfung@gmail.com>
2018-08-15 18:52:54 +0800

    style and favicon

---
+++ js.js
@@ -0,0 +1,4 @@
+// let the divs go full screen centerd
+var outerContainer = document.querySelector("outerContainer");
+outerContainer.width = window.innerWidth ;
+outerContainer.height = window.innerHeight ;
