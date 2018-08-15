cd2b3dd72b08c113ab861554720cca0cbab748b7
峰 <leelapfung@gmail.com>
2018-08-15 19:01:02 +0800

    body height

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
+// let the body goes full screen centerd
+var body = document.querySelector("body");
+body.width = window.innerWidth ;
+body.height = window.innerHeight ;
+
+console.log("js.js works somehow.");
+
+
 // let the divs go full screen centerd
 var outerContainer = document.querySelector("outerContainer");
 outerContainer.width = window.innerWidth ;
